b6a9aeb7c91606012e65b3ecee7eba67114e13c3
Shivang <shivangptl099@gmail.com>
2024-12-27 14:57:23 +0530

    AuthCheck funtion added in backend

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -178,3 +178,12 @@ export const updateProfile=async (req,res)=>
         if(file) fs.unlinkSync(file.path);
     }
 }
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch(error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signup, login, logout, updateProfile } from "../Controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, checkAuth } from "../Controllers/auth.controller.js";
 import { auth } from "../Middleware/auth.middleware.js";
 import { upload } from "../Config/Multer.js";
 
@@ -12,5 +12,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile",upload.single("image"),auth,updateProfile);
+
+router.get("/check", auth, checkAuth);
 
 export default router;
